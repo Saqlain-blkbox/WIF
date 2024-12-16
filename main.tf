11921156572556773d0deb63e0c3d06ddf67f189
Saqlain-blkbox <mohammed.saqlain@blkbox.ai>
2024-12-17 00:08:39 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
-resource "google_storage_bucket" "wif-bucket" {
-  name          = "wif-storage-bucket"
-  location      = "us-central1"
-  project       = "tt-dev-001"
-  public_access_prevention = "enforced"
+terraform {
+  backend "gcs" {
+    bucket  = "wif-tfstate"
+    prefix  = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "blkbox-practice"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "wif-storage-001" {
+  name                     = "example-bucket-${random_id.unique_id.hex}"
+  location                 = "us-central1"
+  uniform_bucket_level_access = true
 }
