08d99d5b3dd859b0d0793cafd3d3dfbc5b3168bf
Saqlain-blkbox <mohammed.saqlain@blkbox.ai>
2024-12-19 12:32:39 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "wif-storage-001" {
-  name                     = "example-bucket-${random_id.unique_id.hex}"
+  name                     = "wiftest-bucket-${random_id.unique_id.hex}"
   location                 = "us-central1"
   uniform_bucket_level_access = true
 }
